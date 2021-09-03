ccc3aa1dbfba7acfceae3e39848c1bc5e125cfd1
Vaibhav  Kumar <vaibhav.kumar@hevodata.com>
2021-09-03 10:40:14 +0530

    Used Base64 Converter in Client

--- googleOAuthLoginDemo/src/main/java/com/example/client/GCSClient.java
+++ googleOAuthLoginDemo/src/main/java/com/example/client/GCSClient.java
@@ -1,41 +1,51 @@
 package com.example.client;
-import com.example.LoginDemoApplication;
-import com.google.cloud.vision.v1.*;
-import com.google.protobuf.ByteString;
-import org.apache.commons.codec.binary.Base64;
 
+import com.example.services.PlantService;
+import com.google.cloud.vision.v1.*;
+import com.google.inject.Inject;
+import com.google.protobuf.ByteString;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Interacts with Google Cloud Systems and fetches the response.
+ */
 public class GCSClient {
 
-    public static void main(final String[] args) throws Exception {
-        System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
-        detectProperties();
-    }
-    public static void detectProperties() throws IOException {
-        String filePath = "/Users/vaibhavkumar/Desktop/Cactus.jpeg";
-        detectProperties(filePath);
+    //TODO : For Testing Replace with method {@processBase64Image} later.
+    public static void main(String[] args) throws IOException {
+
+        processImageFromFilePath("/Users/vaibhavkumar/Desktop/Cactus.jpeg");
     }
 
-    // Detects image properties such as color frequency from the specified local image.
-    public static void detectProperties(String filePath) throws IOException {
-        byte[] imageData = Base64.encodeBase64(filePath.getBytes());
+    public static void processBase64Image(byte[] base64Image) throws IOException {
+        ByteString imgBytesBase64 = ByteString.copyFrom(base64Image);
+        Image img = Image.newBuilder().setContent(imgBytesBase64).build();
+        detectProperties(img);
+    }
+
+    public static void processImageFromFilePath(String filePath) throws IOException {
+        File file = new File(filePath);
+        byte[] imageData = readContentIntoByteArray(file);
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        ByteString byteString = ByteString.readFrom(new FileInputStream(filePath));
+        Image img = Image.newBuilder().setContent(byteString).build();
+        detectProperties(img);
+    }
+
+
+    public static void detectProperties(Image img) throws IOException {
         List<AnnotateImageRequest> requests = new ArrayList<>();
 
-        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
-
-        Image img = Image.newBuilder().setContent(imgBytes).build();
         Feature feat = Feature.newBuilder().setType(Feature.Type.IMAGE_PROPERTIES).build();
         AnnotateImageRequest request =
                 AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
         requests.add(request);
 
-        // Initialize client that will be used to send requests. This client only needs to be created
-        // once, and can be reused for multiple requests. After completing all of your requests, call
-        // the "close" method on the client to safely clean up any remaining background resources.
 
         try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
             BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
@@ -48,23 +58,25 @@ public class GCSClient {
                 }
 
                 // For full list of available annotations, see http://g.co/cloud/vision/docs
-                DominantColorsAnnotation colors = res.getImagePropertiesAnnotation().getDominantColors();
-                //DominantColorsAnnotation colors = res.getImagePropertiesAnnotation().getDominantColors();
-
                 if (res.getLocalizedObjectAnnotationsList().isEmpty()) {
-                    float score_green = 0;
-                    for (ColorInfo color : colors.getColorsList()) {
-                        score_green += color.getColor().getGreen();
-
-                        System.out.println(score_green);
-                    }
-                    System.out.println("score_green");
+                    DominantColorsAnnotation colors = res.getImagePropertiesAnnotation().getDominantColors();
+                    System.out.println(colors.getColors(1));
                 }
             }
         }
     }
 
-    private void detectViaColor() {
-
+    private static byte[] readContentIntoByteArray(File file) {
+        FileInputStream fileInputStream = null;
+        byte[] bFile = new byte[(int) file.length()];
+        try {
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bFile;
     }
+
 }
--- googleOAuthLoginDemo/src/main/java/com/example/services/PlantService.java
+++ googleOAuthLoginDemo/src/main/java/com/example/services/PlantService.java
@@ -3,6 +3,7 @@ package com.example.services;
 import com.example.daos.PlantDAO;
 import com.google.inject.Inject;
 
+
 public class PlantService {
 
     private PlantDAO plantDAO;
